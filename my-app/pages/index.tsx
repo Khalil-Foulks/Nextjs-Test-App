d5ea3141c9159b8473ff8d1a79a86a579272fdb1
Khalil <khalilfoulks@gmail.com>
2021-06-22 13:10:12 -0400

    added data fetching example

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -1,14 +1,27 @@
 import Head from 'next/head'
+import Article from '../components/Article'
 
 
-export default function Home() {
+export default function Home({articles}) {
   return (
     <div>
       <Head>
         <title>Web Dev News</title>
         <meta name='keywords' content='web development, programming'></meta>  
       </Head>
-      <h1>Welcome to Next</h1>
+        <Article articles={articles} />
     </div>
   )
+}
+
+// Fetching data
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const articles = await res.json()
+
+  return {
+    props: {
+      articles
+    }
+  }
 }
